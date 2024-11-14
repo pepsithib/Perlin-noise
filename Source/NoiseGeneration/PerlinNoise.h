e4ddae10ada2f14840f0255208255cb87fdf4e80
Séry Léo <74504632+LeoSery@users.noreply.github.com>
2024-11-14 16:29:31 +0100

    Add: Base class for widget implementation

---
+++ Source/NoiseGeneration/PerlinNoise.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PerlinNoise.generated.h"
+
+UCLASS()
+class NOISEGENERATION_API UPerlinNoise : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+
+	
+};
